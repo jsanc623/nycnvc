6604f573b3b4b87528eff6a6c021ae59cc06dfcd
Doria Penciuc <doria.penciuc@borderfree.com>
2015-10-05 18:17:33 -0400

    Added contact form.

--- wp-content/themes/awaken/layouts/home.php
+++ wp-content/themes/awaken/layouts/home.php
@@ -8,6 +8,24 @@
  */
 
 get_header(); ?>
+<div class="row">
+<?php is_rtl() ? $rtl = 'awaken-rtl' : $rtl = ''; ?>
+<div class="col-xs-12 col-sm-12 col-md-8 <?php echo $rtl ?>">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
 
 
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .bootstrap cols -->
+<div class="col-xs-12 col-sm-6 col-md-4">
+	<?php get_sidebar(); ?>
+</div><!-- .bootstrap cols -->
+</div><!-- .row -->
 <?php get_footer(); ?>
