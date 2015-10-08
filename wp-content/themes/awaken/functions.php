297d0439963e893bcdc28ed5d27856ed5977992e
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2015-10-08 13:02:31 -0400

    Add Redux Framework plugin, add Event List plugin, upgrade Akismet plugin, add menu active highlight modification, add advanced custom fields plugin

--- wp-content/themes/awaken/functions.php
+++ wp-content/themes/awaken/functions.php
@@ -7,6 +7,18 @@
 
 
 /**
+ * Give the active menu an "active" class
+ */
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+     if( in_array('current-menu-item', $classes) ){
+             $classes[] = 'active ';
+     }
+     return $classes;
+}
+
+
+/**
  * Load the TGM init if it exists
  */
 if ( file_exists( get_template_directory() . '/inc/options/tgm/tgm-init.php') ) {
